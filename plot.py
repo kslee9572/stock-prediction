439954ab5a9226f2c393f5e31a8886b184767034
kslee9572 <kslee9572@gmail.com>
2021-03-01 23:27:29 +0900

    contour plot done

--- plot.py
+++ plot.py
@@ -1,1 +1,16 @@
 # returns a heat map as a result
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot(x_max, y_max, data):
+    x = np.arange(10, x_max + 1, 10)
+    y = np.arange(10, y_max + 1, 10)
+
+    plot = plt.contourf(x, y, data, cmap=plt.cm.coolwarm)
+    plt.title("Accuracy")
+    plt.ylabel("Forecast Days")
+    plt.xlabel("Timesteps")
+
+    plt.show()
