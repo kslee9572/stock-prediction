f853a924552b879ccbdab65a3918f692ae41ef33
kslee9572 <kslee9572@gmail.com>
2021-03-01 23:28:28 +0900

    user report done, need to do variables

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ import numpy as np
 import preprocessing as pp
 import model as md
 import statistics as st
+import plot as plt
 
 """ 
 
@@ -42,13 +43,13 @@ def main():
     train_forward = 0
     n_training = 2000
     timesteps = 0
-    repeat = 4
+    repeat = 1
 
     all_data = np.zeros((10, 10, repeat))
     final_data = np.zeros((10, 10))
 
-    for k in range(10):
-        for j in range(10):
+    for k in range(1):
+        for j in range(1):
 
             timesteps = 10 * (j + 1)
             forecast_days = 10 * (k + 1)
@@ -77,6 +78,22 @@ def main():
     # give final_data to plot.py
     # give user statistical report
     ##finsih!!!!
+
+    test_data = [
+        [36.0, 46.0, 54.0, 58.5, 24.5, 57.5, 63.0, 48.0, 52.0, 58.5],
+        [52.0, 43.5, 54.5, 49.5, 44.0, 45.0, 58.5, 38.0, 69.0, 59.5],
+        [33.5, 35.0, 56.0, 47.0, 61.0, 49.0, 63.0, 56.0, 57.5, 49.5],
+        [33.0, 44.5, 48.5, 53.5, 49.5, 59.5, 64.0, 62.5, 49.5, 42.5],
+        [40.0, 41.0, 53.0, 46.0, 55.0, 59.0, 46.5, 56.0, 51.0, 52.0],
+        [28.0, 39.5, 53.5, 57.0, 60.5, 59.5, 58.0, 57.0, 69.0, 57.0],
+        [32.5, 44.0, 42.5, 57.0, 59.5, 56.5, 61.0, 56.0, 66.0, 65.5],
+        [34.0, 31.0, 47.0, 52.5, 56.5, 60.0, 63.0, 37.0, 52.5, 62.0],
+        [27.0, 41.5, 56.0, 54.5, 57.0, 41.0, 61.0, 51.5, 56.5, 65.0],
+        [28.0, 35.0, 50.0, 50.0, 60.0, 60.0, 45.0, 52.5, 64.5, 56.0],
+    ]
+    final_data = np.array(test_data)
+    dt.report(final_data, user_input)
+    plt.plot(100, 100, final_data)
 
 
 ##
